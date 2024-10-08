12b3dbcf0128cd91528a557b6ce43ab5e61fb661
Cristian <389336@alu.murciaeduca.es>
2024-10-08 17:51:32 +0200

    Fin de sección 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  public heroNames: String[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+  public deletedHero?: String;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
+
+
+
+
+}
